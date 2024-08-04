e2517b0263dd22f2ce45982503c0d2c2ea54fa7c
Umer <mac@Onyx-Mac.local>
2024-08-04 23:24:14 +0500

    Added products api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
 
 const app = express()
+app.use(express.json())
 
 mongoose.connect("mongodb+srv://umerqaisar786:od5Afg5fsj94ystf@nodeapi.jl6a4iy.mongodb.net/?retryWrites=true&w=majority&appName=NodeApi")
     .then(() => {
@@ -15,6 +17,21 @@ mongoose.connect("mongodb+srv://umerqaisar786:od5Afg5fsj94ystf@nodeapi.jl6a4iy.m
         console.log("Connection failed. " + e)
     })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/api/products', async (req, res) => {
+    try {
+        const response = await Product.find({})
+        res.status(200).send(response)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
 })
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const response = await Product.create(req.body)
+        res.status(200).send(response)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
