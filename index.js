2c5501e6ce63eafcf26c40d4ef2320db616c26b0
mac <mac@Onyx-Mac.local>
2024-08-04 22:21:04 +0500

    Added mongodb

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log('Express server is running on port 3000')
-})
+mongoose.connect("mongodb+srv://umerqaisar786:od5Afg5fsj94ystf@nodeapi.jl6a4iy.mongodb.net/?retryWrites=true&w=majority&appName=NodeApi")
+    .then(() => {
+        console.log("Connected to mongo server...")
+
+        app.listen(3000, () => {
+            console.log('Express server is running on port 3000')
+        })
+    })
+    .catch((e) => {
+        console.log("Connection failed. " + e)
+    })
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
