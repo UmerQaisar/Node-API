4d461b7ba34bc4464a9738750ada36324b0db1bd
Umer <umerqaisar786@gmail.com>
2024-08-05 14:14:42 +0500

    Updated get product api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Product = require('./models/product.model.js')
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 mongoose.connect("mongodb+srv://umerqaisar786:od5Afg5fsj94ystf@nodeapi.jl6a4iy.mongodb.net/?retryWrites=true&w=majority&appName=NodeApi")
     .then(() => {
@@ -29,8 +30,11 @@ app.get('/api/products', async (req, res) => {
 app.get('/api/product/:id', async (req, res) => {
     try {
         const {id} = req.params
-        const response = await Product.findById(id)
-        res.status(200).send(response)
+        const product = await Product.findById(id)
+        if (!product) {
+            return res.status(400).send('Product not found')
+        }
+        res.status(200).send(product)
     } catch (e) {
         res.status(500).send(e.message)
     }
